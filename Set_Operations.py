6140b06371240f4a582ad792761be023d2ceb405
Nallu-Swami <aringopalan13489@gmail.com>
2024-08-14 15:27:21 +0530

    Set Operations on Images

--- Set_Operations.py
+++ Set_Operations.py
@@ -2,18 +2,19 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-def add_images():
+
+def union_images():
     image_1 = cv.imread("image_1.jpg", cv.IMREAD_COLOR)
     image_2 = cv.imread("image_2.jpg", cv.IMREAD_COLOR)
     if image_1.shape != image_2.shape:
         print("Images are not the same size, resizing...")
         image_2 = cv.resize(image_2, (image_1.shape[1], image_1.shape[0]))    
     
-    result_add_images = cv.add(image_1, image_2)
+    result_add_images = cv.bitwise_or(image_1, image_2)
     
     return result_add_images
 
-def sub_images():
+def intersection_images():
     image_1 = cv.imread("image_1.jpg", cv.IMREAD_COLOR)
     image_2 = cv.imread("image_2.jpg", cv.IMREAD_COLOR)
     
@@ -21,10 +22,10 @@ def sub_images():
         print("Images are not the same size, resizing...")
         image_2 = cv.resize(image_2, (image_1.shape[1], image_1.shape[0]))
         
-    result_subtract_images = cv.subtract(image_1, image_2)
+    result_subtract_images = cv.bitwise_and(image_1, image_2)
     return result_subtract_images
 
-def mul_images():
+def negation_images():
     image_1 = cv.imread("image_1.jpg", cv.IMREAD_COLOR)
     image_2 = cv.imread("image_2.jpg", cv.IMREAD_COLOR)
     
@@ -32,10 +33,10 @@ def mul_images():
         print("Images are not the same size, resizing...")
         image_2 = cv.resize(image_2, (image_1.shape[1], image_1.shape[0]))
         
-    result_multiplied_images = cv.multiply(image_1, image_2)
+    result_multiplied_images = cv.bitwise_not(image_1, image_2)
     return result_multiplied_images
 
-def div_images():
+def inverter_images():
     image_1 = cv.imread("image_1.jpg", cv.IMREAD_COLOR)
     image_2 = cv.imread("image_2.jpg", cv.IMREAD_COLOR)
     
@@ -43,10 +44,10 @@ def div_images():
         print("Images are not the same size, resizing...")
         image_2 = cv.resize(image_2, (image_1.shape[1], image_1.shape[0]))
         
-    result_divided_images = cv.divide(image_1, image_2)
+    result_divided_images = cv.bitwise_xor(image_1, image_2)
     return result_divided_images
 
-result = mul_images()
+result = negation_images()
 
 result_rgb = cv.cvtColor(result, cv.COLOR_BGR2RGB)
 
