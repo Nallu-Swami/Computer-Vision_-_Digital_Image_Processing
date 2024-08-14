47751375d681f4f7bb0de0ce72262232fb4d65f6
Nallu-Swami <aringopalan13489@gmail.com>
2024-08-14 12:45:32 +0530

    Multiple Plotting of Arithmetic Outputs

--- Arithmetic_Operations.py
+++ Arithmetic_Operations.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
-#Reading Images -> Resizing Images -> Image Operation -> GUI representation
+import matplotlib.pyplot as plt
+
 def add_images():
     image_1 = cv.imread("image_1.jpg", cv.IMREAD_COLOR)
     image_2 = cv.imread("image_2.jpg", cv.IMREAD_COLOR)
@@ -12,7 +13,6 @@ def add_images():
     
     return result_add_images
 
-
 def sub_images():
     image_1 = cv.imread("image_1.jpg", cv.IMREAD_COLOR)
     image_2 = cv.imread("image_2.jpg", cv.IMREAD_COLOR)
@@ -21,10 +21,8 @@ def sub_images():
         print("Images are not the same size, resizing...")
         image_2 = cv.resize(image_2, (image_1.shape[1], image_1.shape[0]))
         
-    
     result_subtract_images = cv.subtract(image_1, image_2)
     return result_subtract_images
-
 
 def mul_images():
     image_1 = cv.imread("image_1.jpg", cv.IMREAD_COLOR)
@@ -34,7 +32,6 @@ def mul_images():
         print("Images are not the same size, resizing...")
         image_2 = cv.resize(image_2, (image_1.shape[1], image_1.shape[0]))
         
-    
     result_multiplied_images = cv.multiply(image_1, image_2)
     return result_multiplied_images
 
@@ -49,9 +46,10 @@ def div_images():
     result_divided_images = cv.divide(image_1, image_2)
     return result_divided_images
 
+result = mul_images()  # Replace with add_images(), sub_images(), or mul_images() as needed
 
+result_rgb = cv.cvtColor(result, cv.COLOR_BGR2RGB)
 
-
-cv.imshow("Result of Arithmetic Addition",mul_images())
-cv.waitKey(0)
-cv.destroyAllWindows()
+plt.imshow(result_rgb)
+plt.axis('off')
+plt.show()
